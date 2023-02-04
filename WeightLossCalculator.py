060b39d83a174c62f87c1fc3d3748630d0df7958
ashlinrajangabriel <71674232+ashlinrajangabriel@users.noreply.github.com>
2023-02-04 13:33:46 +0530

    Create WeightLossCalculator.py

---
+++ WeightLossCalculator.py
@@ -0,0 +1,32 @@
+def weight_loss(weight, speed, terrain, calories_burned_per_mile, oats_calories, days):
+    # Calculate number of miles walked
+    miles = speed * 5
+    # Multiply miles with terrain factor to get calories burned per day
+    if terrain == "Flat":
+        calories_burned = miles * calories_burned_per_mile
+    elif terrain == "Hilly":
+        calories_burned = miles * calories_burned_per_mile * 1.5
+    elif terrain == "Uneven":
+        calories_burned = miles * calories_burned_per_mile * 2
+    elif terrain == "Muddy":
+        calories_burned = miles * calories_burned_per_mile * 2.5
+    elif terrain == "Rocky":
+        calories_burned = miles * calories_burned_per_mile * 3
+    else:
+        return "Invalid terrain type"
+    # Calculate the total calories burned for the week
+    total_calories_burned = calories_burned * days
+    # Calculate the net calories
+    net_calories = oats_calories - total_calories_burned
+    # Calculate the weight loss
+    weight_loss = net_calories / 3500
+    return weight - weight_loss
+
+weight = 110
+speed = 5
+terrain = "Hilly"
+calories_burned_per_mile = 100
+oats_calories = 2300
+days = 5
+
+print("Weight loss per week:", weight_loss(weight, speed, terrain, calories_burned_per_mile, oats_calories, days), "kg")
